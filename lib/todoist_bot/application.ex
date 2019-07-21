f8a9b8505f04453d806b4c8259e37d8665df5d14
martosaur <martosaur@gmail.com>
2019-07-21 13:29:52 +0200

    add webhook

--- lib/todoist_bot/application.ex
+++ lib/todoist_bot/application.ex
@@ -2,14 +2,21 @@ defmodule TodoistBot.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      TodoistBot.Poller,
+    router =
       Plug.Cowboy.child_spec(
         scheme: :http,
         plug: TodoistBot.Api,
         options: [port: Application.fetch_env!(:todoist_bot, :app_port)]
       )
-    ]
+
+    children =
+      case TodoistBot.Webhook.setup_webhook() do
+        :ok ->
+          [router]
+
+        {:error, _} ->
+          [TodoistBot.Poller, router]
+      end
 
     opts = [strategy: :one_for_one, name: TodoistBot.Supervisor]
     Supervisor.start_link(children, opts)

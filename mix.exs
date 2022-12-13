20d8d2e3bc6b726716f895384b5409ea818cb7e3
martosaur <martosaur@gmail.com>
2022-12-12 21:20:25 -0800

    Switch from Sync API v8 to v9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TodoistBot.MixProject do
   def project do
     [
       app: :todoist_bot,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()

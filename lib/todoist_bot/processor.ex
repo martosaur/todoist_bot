37f3bbaf18bdb47ab0275ff2ba008836f6696f4b
martosaur <martosaur@gmail.com>
2023-11-23 18:24:13 -0800

    Remove dbg

--- lib/todoist_bot/processor.ex
+++ lib/todoist_bot/processor.ex
@@ -13,7 +13,6 @@ defmodule TodoistBot.Processor do
       |> TodoistBot.Commands.match()
       |> save_user_state()
       |> send_response()
-      |> dbg()
     rescue
       error -> Logger.warning(inspect(error))
     end

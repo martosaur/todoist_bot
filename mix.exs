711380787a8c6ce0347a10116c4ad8fbd21c23d6
martosaur <martosaur@gmail.com>
2024-11-11 10:07:35 -0800

    Version 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TodoistBot.MixProject do
   def project do
     [
       app: :todoist_bot,
-      version: "1.0.4",
+      version: "2.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
